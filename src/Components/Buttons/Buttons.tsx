ef65d79a393e43276b56ca25860a341bb49d53a4
frostzt <aidenfrostbite@gmail.com>
2021-09-15 20:38:16 +0530

    done with header, working on Header Animations

--- src/Components/Buttons/Buttons.tsx
+++ src/Components/Buttons/Buttons.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropsTypes from "prop-types";
 
 // Styling
 import cx from "classnames";
@@ -7,24 +6,14 @@ import styles from "./Buttons.module.scss";
 
 interface Props {
   invert?: boolean;
+  extraClass?: string;
   children: React.ReactNode;
 }
 
-const Buttons: React.FC<Props> = ({ children, invert }) => (
-  <div className={cx([invert ? styles.inverted : styles.container])}>
+const Buttons: React.FC<Props> = ({ children, invert, extraClass }) => (
+  <div className={cx([invert ? styles.inverted : styles.container, extraClass || null])}>
     <div>{children}</div>
   </div>
 );
-
-// Prop Validation
-Buttons.defaultProps = {
-  invert: false,
-  children: "",
-};
-
-Buttons.propTypes = {
-  invert: PropsTypes.bool,
-  children: PropsTypes.node,
-};
 
 export default Buttons;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,20 +2,41 @@ import React from "react";
 
 // Styling
 import cx from "classnames";
+import { motion } from "framer-motion";
 import styles from "./Header.module.scss";
 
 // Components
 import NavBar from "../NavBar/NavBar";
+import Buttons from "../Buttons/Buttons";
 
 const Header: React.FC = () => (
   <header className={styles.container}>
     <NavBar />
-    <div className={styles.main}>
-      <h1 className={cx(["heading-primary", styles.title])}>
+    <motion.div className={styles.main}>
+      <motion.h1 className={cx(["heading-primary", styles.title])}>
         Why be a<br />
         SuperDev?
-      </h1>
-    </div>
+      </motion.h1>
+      <motion.div className={styles.content}>
+        <motion.p className={styles.paragraph}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt a accusamus voluptates commodi
+          iusto sed laborum rem assumenda nulla illo iure, unde error? Consequatur natus expedita delectus. Sunt, est.
+        </motion.p>
+        <motion.p className={styles.paragraph}>
+          Eos explicabo sunt asperiores sed maxime obcaecati quos. Quibusdam eligendi consequuntur recusandae, vero qui
+          impedit similique culpa non nam repellat voluptatem ad!
+        </motion.p>
+        <motion.p className={styles.paragraph}>
+          Eum sapiente eaque culpa atque eius tempora cupiditate quae vel, magni, quidem reiciendis perferendis ut,
+          voluptatum commodi minima expedita asperiores iure labore!
+        </motion.p>
+      </motion.div>
+      <motion.div>
+        <Buttons invert extraClass={styles.button}>
+          Try it out
+        </Buttons>
+      </motion.div>
+    </motion.div>
   </header>
 );
 
